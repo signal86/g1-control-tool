4366c940c9c27f33a28ad31ce6c1919c7d5f6653
signal86 <parathenium@proton.me>
2025-11-19 11:28:50 -0600

    socket doesnt vanish anymore

--- server/server.py
+++ server/server.py
@@ -1,31 +1,32 @@
 import atexit
 import socket
 
-from unitree_sdk2py.core.channel import ChannelSubscriber, ChannelFactoryInitialize
-from unitree_sdk2py.idl.default import unitree_go_msg_dds__SportModeState_
-from unitree_sdk2py.idl.unitree_go.msg.dds_ import SportModeState_
-from unitree_sdk2py.g1.loco.g1_loco_client import LocoClient
-from unitree_sdk2py.g1.arm.g1_arm_action_client import G1ArmActionClient
-from unitree_sdk2py.g1.arm.g1_arm_action_client import action_map
-from unitree_sdk2py.g1.audio.g1_audio_client import AudioClient
-
-ChannelFactoryInitialize()
-
-motion_client = LocoClient()
-motion_client.SetTimeout(10.0)
-motion_client.Init()
-
-arm_client = G1ArmActionClient()
-arm_client.SetTimeout(10.0)
-arm_client.Init()
-
-audio_client = AudioClient()
-audio_client.SetTimeout(10.0)
-audio_client.Init()
-
 # localhost
 # Only have as true if you're running the socket on your local machine
 debug = False
+
+if not debug: 
+    from unitree_sdk2py.core.channel import ChannelSubscriber, ChannelFactoryInitialize
+    from unitree_sdk2py.idl.default import unitree_go_msg_dds__SportModeState_
+    from unitree_sdk2py.idl.unitree_go.msg.dds_ import SportModeState_
+    from unitree_sdk2py.g1.loco.g1_loco_client import LocoClient
+    from unitree_sdk2py.g1.arm.g1_arm_action_client import G1ArmActionClient
+    from unitree_sdk2py.g1.arm.g1_arm_action_client import action_map
+    from unitree_sdk2py.g1.audio.g1_audio_client import AudioClient
+
+    ChannelFactoryInitialize()
+
+    motion_client = LocoClient()
+    motion_client.SetTimeout(10.0)
+    motion_client.Init()
+
+    arm_client = G1ArmActionClient()
+    arm_client.SetTimeout(10.0)
+    arm_client.Init()
+
+    audio_client = AudioClient()
+    audio_client.SetTimeout(10.0)
+    audio_client.Init()
 
 CONFIG = {
     # "hostname": "192.168.0.66",
@@ -42,61 +43,64 @@ s.listen(1)
 
 print("wait")
 try:
-    conn, addr = s.accept()
-    print(f"connected: {conn}")
-
     while True:
-        data = conn.recv(1024)
-        print(f"received: {data.decode()}")
+        conn, addr = s.accept()
+        print(f"connected: {conn}")
 
-        x = 0
-        y = 0
-        z = 0
+        try:
+            while True:
+                data = conn.recv(1024)
+                print(f"received: {data.decode()}")
 
-        if data.decode().startswith("command "):
-            d = data.decode().split('command ', 1)
-            command = d[1]
-            arm_client.ExecuteAction(action_map.get(command))
-            print("Command executed:", command)
-            continue
+                x = 0
+                y = 0
+                z = 0
 
-        if data.decode().split(' ')[0] == "led":
-            colors = data.decode().split(' ', 1)[1].split(' ')
-            audio_client.LedControl(int(colors[0]), int(colors[1]), int(colors[2]))
-            print("Colors changed:", colors)
-            continue
+                if data.decode().startswith("command "):
+                    d = data.decode().split('command ', 1)
+                    command = d[1]
+                    if not debug: arm_client.ExecuteAction(action_map.get(command))
+                    print("Command executed:", command)
+                    continue
 
-        data = data.decode().split()
-        for movement in data:
-            if movement == 'forward':
-                print("Moving forward")
-                x = 0.25
+                if data.decode().split(' ')[0] == "led":
+                    colors = data.decode().split(' ', 1)[1].split(' ')
+                    if not debug: audio_client.LedControl(int(colors[0]), int(colors[1]), int(colors[2]))
+                    print("Colors changed:", colors)
+                    continue
 
-            if movement == 'back':
-                print("Moving backwards")
-                x = -0.25
+                data = data.decode().split()
+                for movement in data:
+                    if movement == 'forward':
+                        print("Moving forward")
+                        x = 0.25
 
-            if movement == 'left':
-                print("Moving left")
-                y = 0.25
+                    if movement == 'back':
+                        print("Moving backwards")
+                        x = -0.25
 
-            if movement == 'right':
-                print("Moving right")
-                y = -0.25
+                    if movement == 'left':
+                        print("Moving left")
+                        y = 0.25
 
-            if movement == 'rotleft':
-                print("Rotating left")
-                z = 0.5
+                    if movement == 'right':
+                        print("Moving right")
+                        y = -0.25
 
-            if movement == 'rotright':
-                print("Rotating right")
-                z = -0.5
+                    if movement == 'rotleft':
+                        print("Rotating left")
+                        z = 0.5
 
-        motion_client.Move(x, y, z)
+                    if movement == 'rotright':
+                        print("Rotating right")
+                        z = -0.5
 
-        if not data: break
+                if not debug: motion_client.Move(x, y, z)
 
-    conn.close()
+                if not data: break
+
+        finally:
+            conn.close()
 
 except socket.error as e:
     print(f"error: {e}")
